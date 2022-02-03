e57825afc5dedacdc8bbb1b94d94a5b6c3b0f8a5
AltSerna <80049269+AltSerna@users.noreply.github.com>
2022-02-03 00:42:29 -0500

    Move&Cam (#1)
    Co-authored-by: Alejandro Serna <chibiserna8@gmail.com>

---
+++ Masquerade/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+    public float mouseSensivity;
+    float xAxisClamp=0;
+    public Transform playerBody;
+
+    private void Awake()
+    {
+        LockCursor();
+    }
+
+    void Update()
+    {
+        CameraRotation();
+    }
+
+    void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void CameraRotation()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensivity * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensivity * Time.deltaTime;
+
+        xAxisClamp += mouseY;
+
+        if(xAxisClamp>90)
+        {
+            xAxisClamp = 90;
+            mouseY = 0;
+            ClampxAxisRotationToValue(270);
+        }
+        else if (xAxisClamp < -90)
+        {
+            xAxisClamp = -90;
+            mouseY = 0;
+            ClampxAxisRotationToValue(90);
+        }
+
+        transform.Rotate(Vector3.left * mouseY);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+
+    void ClampxAxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+}
---
+++ Masquerade/Assets/Scripts/PlayerMovemnt.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovemnt : MonoBehaviour
+{
+    CharacterController chController;
+    public float movementSpeed;
+    float ActualSpeed;
+
+    private void Start()
+    {
+        chController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        PlayerMovement();
+
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            movementSpeed = movementSpeed * 2;
+        }
+
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            movementSpeed = movementSpeed / 2;
+
+        }
+    }
+
+    void PlayerMovement()
+    {
+        float horiInput = Input.GetAxisRaw("Horizontal") * movementSpeed * Time.deltaTime;
+        float vertInput = Input.GetAxisRaw("Vertical") * movementSpeed * Time.deltaTime;
+
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 rightMovement = transform.right * horiInput;
+
+        chController.SimpleMove(forwardMovement + rightMovement);
+    }
+
+}
