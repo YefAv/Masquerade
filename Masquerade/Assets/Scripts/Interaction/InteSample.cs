3bb641ced3cf81f967dc746d04ea6f53a675c79f
jordanbolivarq <60189623+jordanbolivarq@users.noreply.github.com>
2022-02-04 04:35:25 -0500

    interaction (#4)

---
+++ Masquerade/Assets/Scripts/Interaction/InteSample.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteSample : MonoBehaviour, IInteractable
+{
+    public void Select()
+    {
+        Debug.Log("action");
+    }
+}
---
+++ Masquerade/Assets/Scripts/Interaction/InteractionManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionManager : MonoBehaviour
+{
+    [SerializeField] float maxRange = 3.5f;
+    [SerializeField] LayerMask mask;
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    void Update()
+    {
+        Selecting();
+    }
+    void Selecting()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, maxRange, mask))
+            {
+                var selectable = hit.transform;
+                IInteractable selectableCode = selectable.gameObject.GetComponent<IInteractable>();
+                if (selectableCode != null)
+                {
+                    selectableCode.Select();
+                }
+            }
+        }
+    }
+}
