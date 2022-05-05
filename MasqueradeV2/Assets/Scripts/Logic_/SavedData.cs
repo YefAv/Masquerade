cdcd6a58b0c032ae6f642c7fbcd77f454bf0423a
AltSerna <alsego8@gmail.com>
2022-05-05 00:37:55 -0500

    Chimbadas

--- MasqueradeV2/Assets/Scripts/Logic_/SavedData.cs
+++ MasqueradeV2/Assets/Scripts/Logic_/SavedData.cs
@@ -9,7 +9,7 @@ public class SavedData: MonoBehaviour
     public List<int> mapNotes = new List<int>();
     public List<int> clueNotes = new List<int>();
     //public bool firstTime;
-    public int day = 0;
+    public int day = 12;
     public bool firstBell = false;
     public bool didTutorial;
 }
